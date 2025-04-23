0896b992b1a975f4abcf694ec94ddbff6e2cbeed
Clive <Cliveswe@users.noreply.github.com>
2025-04-23 15:17:12 +0200

    Add project files.

---
+++ SmartBook/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartBook
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("SmartBook: your personal library system");
+            SmartBookApplication smartBookApp = new();
+            smartBookApp.Start();
+        }
+    }
+}
---
+++ SmartBook/SmartBookApplication.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SmartBook;
+
+public class SmartBookApplication
+{
+    internal void Start() {
+        throw new NotImplementedException();
+    }
+}
