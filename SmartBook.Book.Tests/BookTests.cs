4f3fd0a14e597fb05436926b35cddba366751d5f
Clive <Cliveswe@users.noreply.github.com>
2025-04-24 15:14:13 +0200

    Created a test project to test the library sigleton class. Move some test data from book tests to library tests.

--- SmartBook.Book.Tests/BookTests.cs
+++ SmartBook.Book.Tests/BookTests.cs
@@ -4,60 +4,6 @@ namespace SmartBook;
 
 public class BookTests
 {
-    /*
-Title: Lorem Ipsum Chronicles
-Author: Dolor Sit
-Category: Fiction
-ISBN: 978-0-123456-47-2
-
-Title: Adventures of Amet Elit
-Author: Amet Elit
-Category: Fantasy
-ISBN: 978-1-234567-89-7
-
-Title: Sed Do Temporalis
-Author: Incididunt Ut
-Category: Science Fiction
-ISBN: 978-0-321-56789-0
-
-Title: Labore et Dolore: A Mystery
-Author: Magna Aliqua
-Category: Mystery
-ISBN: 978-3-16-148410-0
-
-Title: Ut Enim Veniam
-Author: Quis Nostrud
-Category: Romance
-ISBN: 978-0-262-13472-9
-
-Title: Exercitationem: The Escape
-Author: Laboris Nisi
-Category: Thriller
-ISBN: 978-1-4028-9462-6
-
-Title: Aliquip Commodo Quest
-Author: Duis Consequat
-Category: Adventure
-ISBN: 978-0-395-19395-8
-
-Title: Reprehenderit Voluptate: A Tale
-Author: Velit Esse
-Category: Historical Fiction
-ISBN: 978-0-7432-7356-5
-
-Title: Cillum Dolore Eu
-Author: Fugiat Nulla
-Category: Horror
-ISBN: 978-0-7432-7357-2
-
-Title: Excepteur Sint Biography
-Author: Cupidatat Non
-Category: Biography
-ISBN: 978-1-56619-909-4
-    **/
-
-
-
     [Fact]
     public void CreateANewBookTest() {
 
