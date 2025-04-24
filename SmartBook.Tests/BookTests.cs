07afb98b7430c1fbb1f9857f185c3c14e088c325
Clive <Cliveswe@users.noreply.github.com>
2025-04-24 08:00:18 +0200

    Setting up test for book class and library class.

---
+++ SmartBook.Tests/BookTests.cs
@@ -0,0 +1,56 @@
+﻿namespace SmartBook.Tests;
+
+public class BookTests
+{
+    /*
+Title: Dolor Sit Amet
+Author: Lorem T. Ipsum
+Category: Fiction
+ISBN: 978-1-23456-789-0
+
+Title: Consectetur Adipiscing Elit
+Author: Aenean V. Vitae
+Category: Self-Help
+ISBN: 978-0-98765-432-1
+
+Title: Ut Enim Ad Minim
+Author: Cillum D. Tempor
+Category: Science Fiction
+ISBN: 978-1-11223-344-5
+
+Title: Exercitation Ullamco Laboris
+Author: Magna A. Aliqua
+Category: Thriller
+ISBN: 978-3-45678-901-2
+
+Title: Nisi Ut Aliquip
+Author: Commodo C. Consequat
+Category: Romance
+ISBN: 978-2-12345-678-9
+
+Title: Duis Aute Irure Dolor
+Author: Reprehenderit I. Voluptate
+Category: Mystery
+ISBN: 978-4-56789-012-3
+
+Title: Velit Esse Cillum
+Author: Eu F. Fugiat
+Category: Historical Fiction
+ISBN: 978-5-67890-123-4
+
+Title: Cupidatat Non Proident
+Author: Sunt I. Culpa
+Category: Biography
+ISBN: 978-6-78901-234-5
+
+Title: Officia Deserunt Mollit
+Author: Anim E. Est
+Category: Horror
+ISBN: 978-7-89012-345-6
+
+Title: Laborum Et Dolore
+Author: Fugiat N. Nulla
+Category: Poetry
+ISBN: 978-8-90123-456-7
+**/
+}
