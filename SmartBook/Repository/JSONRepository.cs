87b6575031ae4d0a2d76b2bcdc9f6c4730c32a6c
Clive <Cliveswe@users.noreply.github.com>
2025-04-28 16:28:19 +0200

    Created a JSONRepository class to save and load data to a file.

---
+++ SmartBook/Repository/JSONRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace SmartBook.Repository;
+public class JSONRepository
+{
+
+    private string filePath;
+    private string fileName;
+    private string fileExtension;
+    public JSONRepository(string filePath, string fileName, string fileExtension) {
+        this.filePath = filePath;
+        this.fileName = fileName;
+        this.fileExtension = fileExtension;
+    }
+    public void SaveToFile(string data) {
+        // TODO Implementation for saving data to a JSON file
+    }
+    public string LoadFromFile() {
+        // TODO Implementation for loading data from a JSON file
+        return string.Empty;
+    }
+}
