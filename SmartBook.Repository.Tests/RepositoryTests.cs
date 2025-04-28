09ec099ca71d4b3d77e684e219bf4dc65de031cd
Clive <Cliveswe@users.noreply.github.com>
2025-04-28 16:43:00 +0200

    Added additional field variables to the RepositoryTests.

--- SmartBook.Repository.Tests/RepositoryTests.cs
+++ SmartBook.Repository.Tests/RepositoryTests.cs
@@ -1,19 +1,22 @@
-﻿using System.Text;
-namespace SmartBook.Repository.Tests;
+﻿namespace SmartBook.Repository.Tests;
 
 
 public class RepositoryTests
 {
-
+    private Library library;
     private DirectoryInfo directoryInformationPath;
+    JSONRepository jsonRepository;
+    private readonly string filePath = @"..\..\..\..\Smartbook\Repository";
+    private readonly string fileName = "Library";
+    private readonly string fileExtension = ".json";
 
     /// <summary>
     /// Constructor for the RepositoryTests class.
     /// </summary>
     public RepositoryTests() {
-        StringBuilder sb = new StringBuilder();
-        sb.Append(@"..\..\..\..\Smartbook\Repository");
-        directoryInformationPath = new DirectoryInfo(sb.ToString());
+        directoryInformationPath = new(filePath);
+        jsonRepository = new JSONRepository(filePath, fileName, fileExtension);
+        library = Library.Instance;
     }
 
     [Fact]
