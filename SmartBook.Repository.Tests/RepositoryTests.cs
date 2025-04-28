a36450ab5e6d13d195b45f8c2c192e1bbd6437eb
Clive <Cliveswe@users.noreply.github.com>
2025-04-28 14:46:50 +0200

    Created a new test project to test a repository to store a json file.

---
+++ SmartBook.Repository.Tests/RepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartBook.Repository.Tests;
+
+public class UnitTest1
+{
+    private readonly DirectoryInfo directoryInformationPath = new DirectoryInfo(@"..\..\..\..\Smartbook\Repository");
+    [Fact]
+    public void Test1() {
+
+        var y = directoryInformationPath.GetFiles();
+        Assert.NotNull(y);
+    }
+}
