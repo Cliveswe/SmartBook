b7d8dd76747837ab31cba8a91043395a6e2888e7
Clive <Cliveswe@users.noreply.github.com>
2025-04-30 16:32:00 +0200

    Added a summary to the class methods.

--- SmartBook/Repository/JSONRepository.cs
+++ SmartBook/Repository/JSONRepository.cs
@@ -5,10 +5,26 @@ public class JSONRepository
 
     private readonly DirectoryInfo directoryInformationPath;
 
+    /// <summary>
+    /// The path to the file where the data is stored.
+    /// </summary>
     public static string FilePath => @"..\..\..\..\Smartbook\Data\";
+    /// <summary>
+    /// The name of the file where the data is stored.
+    /// </summary>
     public static string FileName => "Library";
+    /// <summary>
+    /// The file extension of the file where the data is stored.
+    /// </summary>
     public static string FileExtension => ".json";
+    /// <summary>
+    /// The full path to the file where the data is stored.
+    /// </summary>
     public static string PathToFile => Path.Combine(FilePath, FileName + FileExtension);
+
+    /// <summary>
+    /// Constructor for the JSONRepository class.
+    /// </summary>
     public JSONRepository() {
 
         directoryInformationPath = new DirectoryInfo(FilePath);
@@ -20,6 +36,11 @@ public class JSONRepository
             File.Create(PathToFile).Close();
         }
     }
+
+    /// <summary>
+    /// Saves the data to a file in JSON format.
+    /// </summary>
+    /// <param name="data"></param>
     public void SaveToFile(List<LibraryBook> data) {
         ArgumentNullException.ThrowIfNull(data);
 
@@ -27,6 +48,11 @@ public class JSONRepository
         File.WriteAllText(PathToFile, jsonData);
     }
 
+    /// <summary>
+    /// Loads the data from a file in JSON format.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="FileNotFoundException"></exception>
     public List<LibraryBook> LoadFromFile() {
         string data = File.ReadAllText(PathToFile);
         if(!string.IsNullOrEmpty(data)) {
@@ -38,6 +64,9 @@ public class JSONRepository
         throw new FileNotFoundException("File not found or empty.", PathToFile);
     }
 
+    /// <summary>
+    /// Deletes the file if it exists.
+    /// </summary>
     public void DeleteFile() {
         // Remove the file if it exists
         if(File.Exists(PathToFile)) {
