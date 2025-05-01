68bf3d32a65fb8020f45197290cfa1ce0282365e
Clive <Cliveswe@users.noreply.github.com>
2025-05-01 09:09:11 +0200

    Created a logger class. This class is used to log events in the application. The file that the logger creates is a TXT file.

---
+++ SmartBook/Utilities/Log.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+
+namespace SmartBook.Utilities;
+public sealed class Log
+{
+    /// <summary>
+    /// The path to the file where the log is stored.
+    /// </summary>
+    private static string FilePath => @"..\..\..\..\Smartbook\Data\";
+
+    /// <summary>
+    /// The name of the file where the log is stored.
+    /// </summary>
+    private static string FileName => "Log";
+    /// <summary>
+    /// The file extension of the file where the log is stored.
+    /// </summary>
+    private static string FileExtension => ".txt";
+
+    /// <summary>
+    /// The full path to the file where the log is stored.
+    /// </summary>
+    public static string PathToFile => Path.Combine(FilePath, FileName + FileExtension);
+
+    private static readonly Log instance = new Log();
+
+    /// <summary>
+    /// Singleton instance of the Log class.
+    /// </summary>
+    public static Log Instance {
+        get {
+            if(instance != null)
+                return instance;
+            return new Log();
+        }
+
+    }
+
+    /// <summary>
+    /// Logs a message to the log file.
+    /// </summary>
+    /// <param name="message"></param>
+    public void LogMessage(string message) {
+        ArgumentNullException.ThrowIfNull(message);
+        //Open the file in append mode creates the file if it does not already exist.
+        using StreamWriter writer = new StreamWriter(PathToFile, true);
+        writer.WriteLine($"{DateTime.Now} - INFORMATION: {message}");
+    }
+
+    /// <summary>
+    /// Logs an exception to the log file.
+    /// </summary>
+    /// <param name="ex"></param>
+    /// <param name="message">This message is optional.</param>
+    public void LogMessage(Exception ex, string? message = null) {
+        StringBuilder logMessage = new StringBuilder();
+
+        logMessage.AppendLine($"{DateTime.Now} - ERROR: {ex.Message}");
+        if(ex.InnerException != null) {
+            logMessage.AppendLine($" - INNER EXCEPTION: {ex.InnerException.Message}");
+        }
+        if(message != null) {
+            logMessage.AppendLine($" - INFORMATION: {message}");
+        }
+        //Open the file in append mode creates the file if it does not already exist.
+        using StreamWriter writer = new StreamWriter(PathToFile, true);
+        writer.WriteLine(logMessage.ToString());
+    }
+
+
+}
